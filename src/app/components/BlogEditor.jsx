d1b5dce48b83dc9d187949b954027dce2ec43c4e
Muhammad Hassan Raza <153998754+MuhammadHassanRaza25@users.noreply.github.com>
2026-01-10 19:23:55 +0500

    working on Tiptap Editor

--- src/app/components/BlogEditor.jsx
+++ src/app/components/BlogEditor.jsx
@@ -2,6 +2,11 @@
 
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { FaItalic } from "react-icons/fa";
+import { HiBold } from "react-icons/hi2";
+import { RiListUnordered } from "react-icons/ri";
+import { LuListOrdered } from "react-icons/lu";
+import { MdOutlineFormatUnderlined } from "react-icons/md";
 
 export default function BlogEditor({ value, onChange }) {
   const editor = useEditor({
@@ -18,46 +23,56 @@ export default function BlogEditor({ value, onChange }) {
   return (
     <div className="bg-white/10 border border-white/30 focus:outline-none focus:border-emerald-500 rounded-xl text-white">
       {/* Toolbar */}
-      <div className="flex gap-3 py-2 px-5 w-full rounded-tl-xl rounded-tr-xl backdrop-blur-sm bg-emerald-700/30 border-b border-emerald-400 transition-all duration-300">
+      <div className="flex justify-center gap-3 py-2 px-5 w-full rounded-tl-xl rounded-tr-xl backdrop-blur-sm bg-emerald-700/30 border-b border-emerald-400 transition-all duration-300">
         <button
-          className={`px-2 rounded-sm cursor-pointer transition-all duration-200 ease-in-out font-semibold text-sm ${
+          className={`px-2 py-0.5 rounded-sm cursor-pointer transition-all duration-200 ease-in-out font-semibold text-sm ${
             editor.isActive("bold")
               ? "bg-emerald-500 text-white"
               : "bg-gray-200 text-gray-700"
           }`}
           onClick={() => editor.chain().focus().toggleBold().run()}
         >
-          B
+          <HiBold className="text-sm" />
         </button>
         <button
-          className={`px-2 rounded-sm cursor-pointer transition-all duration-200 ease-in-out font-semibold text-sm ${
+          className={`px-2 py-0.5 rounded-sm cursor-pointer transition-all duration-200 ease-in-out font-semibold text-sm ${
             editor.isActive("italic")
               ? "bg-emerald-500 text-white"
               : "bg-gray-200 text-gray-700"
           }`}
           onClick={() => editor.chain().focus().toggleItalic().run()}
         >
-          I
+          <FaItalic className="text-xs" />
+        </button>
+         <button
+          className={`px-2 py-0.5 rounded-sm cursor-pointer transition-all duration-200 ease-in-out font-semibold text-sm ${
+            editor.isActive("italic")
+              ? "bg-emerald-500 text-white"
+              : "bg-gray-200 text-gray-700"
+          }`}
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+        >
+          <MdOutlineFormatUnderlined className="text-base" />
         </button>
         <button
-          className={`px-2 rounded-sm cursor-pointer transition-all duration-200 ease-in-out font-semibold text-sm ${
+          className={`px-2 py-0.5 rounded-sm cursor-pointer transition-all duration-200 ease-in-out font-semibold text-sm ${
             editor.isActive("bulletList")
               ? "bg-emerald-500 text-white"
               : "bg-gray-200 text-gray-700"
           }`}
           onClick={() => editor.chain().focus().toggleBulletList().run()}
         >
-          UL
+          <RiListUnordered className="text-base" />
         </button>
         <button
-          className={`px-2 rounded-sm cursor-pointer transition-all duration-200 ease-in-out font-semibold text-sm ${
+          className={`px-2 py-0.5 rounded-sm cursor-pointer transition-all duration-200 ease-in-out font-semibold text-sm ${
             editor.isActive("orderedList")
               ? "bg-emerald-500 text-white"
               : "bg-gray-200 text-gray-700"
           }`}
           onClick={() => editor.chain().focus().toggleOrderedList().run()}
         >
-          OL
+          <LuListOrdered className="text-lg" />
         </button>
       </div>
 
